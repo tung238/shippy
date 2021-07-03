e8954397f8a37a46b3469c582e7764cd8c6ae0f9
Tung Tran <tung238@gmail.com>
2021-07-03 16:37:53 +0700

    update

--- shippy-cli-consignment/go.mod
+++ shippy-cli-consignment/go.mod
@@ -2,6 +2,8 @@ module github.com/tung238/shippy/shippy-cli-consignment
 
 go 1.16
 
-
-replace github.com/tung238/shippy/shippy-service-consignment => ../shippy-service-consignment
 replace github.com/tung238/shippy/shippy-cli-consignment => ../shippy-cli-consignment
+replace github.com/tung238/shippy/shippy-service-consignment => ../shippy-service-consignment
+
+require github.com/asim/go-micro/v3 v3.5.1 // indirect
+require github.com/tung238/shippy/shippy-service-consignment v0.0.0
--- shippy-cli-consignment/main.go
+++ shippy-cli-consignment/main.go
@@ -9,8 +9,8 @@ import (
 
 	"context"
 
+	"github.com/asim/go-micro/v3"
 	pb "github.com/tung238/shippy/shippy-service-consignment/proto/consignment"
-	"google.golang.org/grpc"
 )
 
 const (
@@ -29,13 +29,12 @@ func parseFile(file string) (*pb.Consignment, error) {
 }
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("Did not connect: %v", err)
-	}
-	defer conn.Close()
-	client := pb.NewShippingServiceClient(conn)
+	service := micro.NewService(
+		micro.Name("shippy.consignment.cli"),
+	)
+	service.Init()
+
+	client := pb.NewShippingService("shippy.consignment.service", service.Client())
 
 	// Contact the server and print out its response.
 	file := defaultFilename
